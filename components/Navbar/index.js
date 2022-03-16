18afdcf83aec1bd3eb4dee1887203fe09be18a1a
YeganaAbd <abd.yegane@gmail.com>
2022-03-16 15:56:00 +0000

    navbar margins

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -5,11 +5,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function NavMenu() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">Yegana Abdullayeva</Navbar.Brand>
+      <Navbar.Brand style={{ marginLeft: "20px" }} href="#home">
+        Yegana Abdullayeva
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/about">About</Nav.Link>
+        <Nav style={{ marginRight: "20px" }} className="ms-auto">
+          <Nav.Link style={{ marginRight: "20px" }} href="/about">
+            About
+          </Nav.Link>
           <Nav.Link href="/projects">Projects</Nav.Link>
         </Nav>
       </Navbar.Collapse>
