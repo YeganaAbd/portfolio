d39e749db6ee9985e087209d6a727b9b9b92d912
YeganaAbd <abd.yegane@gmail.com>
2022-03-22 23:24:32 +0000

    carousel progressing

--- pages/projects.js
+++ pages/projects.js
@@ -5,19 +5,28 @@ import { useState } from "react";
 
 export default function Projects() {
   const [project, setProject] = useState(projects);
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
   return (
     <>
       <NavMenu />
       <div>Hello</div>
-      {project.map((pr, index) => {
-        return (
-          <Carousel key={index}>
-            <h2>{pr.projectName}</h2>
-            <p>{pr.description}</p>
-            <p>{pr.techStack}</p>
-          </Carousel>
-        );
-      })}
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        {project.map((pr, index) => {
+          return (
+            <Carousel.Item key={index}>
+              <h2>{pr.projectName}</h2>
+              <Carousel.Caption>
+                <p>{pr.description}</p>
+                <p>{pr.techStack}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
     </>
   );
 }
