4a9f1f5484cc53def995b01a1e3fad3a625db80c
YeganaAbd <abd.yegane@gmail.com>
2022-03-22 23:51:14 +0000

    pictures of projects

--- ProjectsData/index.js
+++ ProjectsData/index.js
@@ -3,11 +3,15 @@ const projects = [
     projectName: "Tic-tac-toe",
     description: "Game of tic-tac-toe",
     techStack: "Html, Css, Javascript",
+    link: "https://tender-dubinsky-cd8ffb.netlify.app/",
+    src: "/tic-tac-js.png",
   },
   {
     projectName: "Tic-tac-toe",
     description: "Game of tic-tac-toe",
     techStack: "React",
+    link: "https://optimistic-boyd-bacd74.netlify.app/",
+    src: "/tic-tac-react.png",
   },
 ];
 export default projects;
--- pages/projects.js
+++ pages/projects.js
@@ -2,6 +2,7 @@ import NavMenu from "../components/NavMenu";
 import Carousel from "react-bootstrap/Carousel";
 import projects from "../ProjectsData/index.js";
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Projects() {
   const [project, setProject] = useState(projects);
@@ -16,13 +17,20 @@ export default function Projects() {
       <div>Hello</div>
       <Carousel activeIndex={index} onSelect={handleSelect}>
         {project.map((pr, index) => {
+          console.log("src", pr.src);
+
           return (
             <Carousel.Item key={index}>
+              <Image
+                width="50px"
+                height="50px"
+                className="d-block w-100"
+                src={pr.src}
+                alt="First slide"
+              />
               <h2>{pr.projectName}</h2>
-              <Carousel.Caption>
-                <p>{pr.description}</p>
-                <p>{pr.techStack}</p>
-              </Carousel.Caption>
+              <p>{pr.description}</p>
+              <p>{pr.techStack}</p>
             </Carousel.Item>
           );
         })}
