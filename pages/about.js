2c7a77becf0c5479efcd8748dcba81235bfd723b
YeganaAbd <abd.yegane@gmail.com>
2022-03-25 11:42:33 +0000

    type fixed

--- pages/about.js
+++ pages/about.js
@@ -11,13 +11,13 @@ export default function About() {
         <p className={styles.about}>
           Hi, I am Yegana Abdullayeva. I have graduated from Computer
           Engineering in 2012. Later, gained master&apos;s degree in Computer
-          Security from University of Birmingham. I have worked as a lecturer in
-          Cryptography and HCI at a University in Azerbaijan for a few years. I
-          had to leave my work due to relocation with my family. Also because my
-          son was young, I didn&apos;t work for the past 4-5 years. Now I am
-          looking forward to starting my career as a Software Engineer to work
-          on real-world problems and potentially improve people&apos;s lives
-          through software. You can view my resume here:
+          Security from the University of Birmingham. I have worked as a
+          lecturer in Cryptography and HCI at a University in Azerbaijan for a
+          few years. I had to leave my work due to relocation with my family.
+          Also because my son was young, I didn&apos;t work for the past 4-5
+          years. Now I am looking forward to starting my career as a Software
+          Engineer to work on real-world problems and potentially improve
+          people&apos;s lives through software. You can view my resume here:
         </p>
 
         <Link
