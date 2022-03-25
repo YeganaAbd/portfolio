b0eb15121c301d14244a2d2f82d65a648fb011a6
YeganaAbd <abd.yegane@gmail.com>
2022-03-25 11:51:47 +0000

    more projects added

--- ProjectsData/index.js
+++ ProjectsData/index.js
@@ -6,7 +6,7 @@ const projects = [
       "As a team of 6, we came up with the idea of making a full-stack application that makes learning maths fun. The delivery process of the project followed the agile methodologies and took 4 weeks. The application requires users to signup in the beginning. Once signed in, the users can take fun math quizzes, collect jellly beans which is a custom currency that can be used to shop in the app. The users can view their profiles page, as well as the leaderboard page for top users. Started from planning using miro, figma for low and high fidelity designs, NextJs for front-end, Node for backend, Postgres for storage. The backend and front-end applications were deployed in heroku and netlify, respectively. We implemented integration tests for backend using supertest and end-to-end tests for front-end using cypress.",
 
     techStack:
-      "NextJs, Cypress, Javascript, Auth0, PostgreSQL, Express.js, jest, supertest",
+      "NextJs, NodeJs, Javascript, Auth0, PostgreSQL, Express.js, Cypress, jest, supertest",
     link: "https://jellly.netlify.app/",
   },
   {
@@ -27,19 +27,35 @@ const projects = [
   },
   {
     id: 4,
-    projectName: "Tic-tac-toe",
-    description: "Game of tic-tac-toe",
-    techStack: "React",
-    link: "https://optimistic-boyd-bacd74.netlify.app/",
-    src: "/tic-tac-react.png",
+    projectName: "Weather App",
+    description: "Weather App",
+    techStack: "Html, css, Javascript",
+    link: "https://github.com/YeganaAbd/weather-api",
+    src: "",
   },
   {
     id: 5,
-    projectName: "Tic-tac-toe",
-    description: "Game of tic-tac-toe",
+    projectName: "Google Clone",
+    description: "Clone of google page",
+    techStack: "Css",
+    link: "https://github.com/YeganaAbd/Google-clone",
+    src: "",
+  },
+  {
+    id: 6,
+    projectName: "Recipe App",
+    description: "Recipe App. You can view recipes.",
     techStack: "React",
-    link: "https://optimistic-boyd-bacd74.netlify.app/",
-    src: "/tic-tac-react.png",
+    link: "https://github.com/YeganaAbd/recipe_app",
+    src: "",
+  },
+  {
+    id: 7,
+    projectName: "Todo-List",
+    description: "Todo-List app. Practised all skills that is learned.",
+    techStack: "React, NodeJs",
+    link: "https://github.com/YeganaAbd/recipe_app",
+    src: "",
   },
 ];
 export default projects;
