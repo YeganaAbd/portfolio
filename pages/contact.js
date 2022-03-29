ff5d38d489e539d7b4f6f3c8c0ad54569ff9b46b
YeganaAbd <abd.yegane@gmail.com>
2022-03-29 11:02:31 +0100

    changing contact icons

--- pages/contact.js
+++ pages/contact.js
@@ -1,20 +1,8 @@
 import NavMenu from "../components/NavMenu";
 import styles from "../styles/Contact.module.css";
-import Button from "react-bootstrap/Button";
 import Link from "next/link";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  fab,
-  faTwitterSquare,
-  faFacebook,
-  faLinkedin,
-  faGithub,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { FaEnvelope } from "react-icons/fa";
-
+import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
 export default function Contact() {
-  library.add(fab, faTwitterSquare, faFacebook, faLinkedin, faGithub);
   return (
     <>
       <NavMenu />
@@ -28,19 +16,14 @@ export default function Contact() {
         </div>
         <div className={styles.connect}>
           <Link href="https://github.com/YeganaAbd" passHref>
-            <FontAwesomeIcon
-              icon={["fab", "github"]}
-              size="4x"
-              style={{ cursor: "pointer" }}
-            />
+            <FaGithub size={70} style={{ cursor: "pointer" }} />
           </Link>
           <Link
             href="https://www.linkedin.com/in/yegana-abdullayeva-98425645/"
             passHref
           >
-            <FontAwesomeIcon
-              icon={["fab", "linkedin"]}
-              size="4x"
+            <FaLinkedin
+              size={70}
               style={{ color: "#0e76a8", cursor: "pointer" }}
             />
           </Link>
